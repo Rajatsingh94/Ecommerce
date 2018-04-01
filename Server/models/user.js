f920307129f3e7c8b6daf24a978281d44a2bb713
Rajatsingh94 <rajat94.singh@gmail.com>
2018-03-31 22:27:51 -0400

    Image method added to Schema

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
+const crypto = require('crypto');
 
 const UserSchema = new Schema({
 
@@ -20,3 +21,41 @@ const UserSchema = new Schema({
 
 
 });
+
+UserSchema.pre('save', function(next){
+
+    var user = this;
+
+    if(!user.isModified('password')) return next();
+
+    bcrypt.hash('password', function(err){
+        if(err)
+        {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
+
+})
+
+UserSchema.methods.comparePassword = function(password)
+{
+    return bcrypt.compareSync(password,this.password);
+};
+
+UserSchema.methods.gravatar = function(size)
+{
+    if(!size) return size=200;
+
+    if(!email){
+
+        return 'https://gravatar.com/avatar/?s' + size + '&d=retro';
+
+    }
+    else{
+
+        var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+        return 'https://gravatar.com/avatar/'+ md5 + '?s'+ size + '&d=retro';
+    }
+};

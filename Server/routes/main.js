0043aae53c8fd9b04bb098fa4f76c90104ced79b
Rajatsingh94 <rajat94.singh@gmail.com>
2018-04-05 23:49:05 -0400

    updated

--- Server/routes/main.js
+++ Server/routes/main.js
@@ -27,15 +27,68 @@ router.route('/categories')
 router.get('/categories/:id',(req, res, next)=>{
   const perPage = 10;
   const page = req.query.page
-  async.waterfall([
+  async.parallel([
     function(callback)
     {
         Product.count({category: req.params.id}, (err, count)=>{
             var totalProducts = count;
             callback(err, totalProducts);
         });
+    },
+    function(callback)
+    {
+        Product.find({category: req.params.id})
+        .skip(perPage * page)
+        .limit(perPage)
+        .populate('category')
+        .populate('owner')
+        .exec((err, products)=>{
+            if(err) return next(err);
+            callback(err, products, totalProducts);
+        });
+    },
+    function(callback)
+    {
+        Category.findOne({_id: req.params.id}, (err,category)=>{
+           callback(err, category)
+        });
     }
-  ])
+  ], function(err, results){
+      var totalProducts = results[0];
+      var products = results[1];
+      var category = results[2];
+      res.json({
+          success:true,
+          message:'category',
+          products: products,
+          categoryName: category.name,
+          totalProducts: totalProducts,
+          pages: Math.ceil(totalProducts/perPage)
+      });
+  });
+});
+
+router.get('product/:id', (req, res, next)=>{
+    Product.findById({_id:req.params.id})
+    .populate('category')
+    .populate('owner')
+    .exec((err, products)=>{
+        if(err)
+        {
+            res.json({
+                success:false,
+                message: 'Product not found'
+            });
+        }else{
+            if(product)
+            {
+                res.json({
+                    success:true,
+                    product:product
+                });
+            }
+        }
+    });
 });
 
 
